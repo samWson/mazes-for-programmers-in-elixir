4cd835062b7bbe56a07f0b4cdae983c4f586a5fa
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-10-12 14:19:03 +1300

    Create grid of unintialized cells

--- maze.ex
+++ maze.ex
@@ -1,6 +1,12 @@
 defmodule Maze do
   def grid(columns, rows) do
     length = columns * rows
-    List.duplicate(0, length)
+    uninitialized_cell = %{column: 0, row: 0, links: []}
+
+    List.duplicate(uninitialized_cell, length)
+  end
+
+  defp initialize_cells(grid) do
+    # #map over grid then do two nested ranges with #each for columns and rows
   end
 end
