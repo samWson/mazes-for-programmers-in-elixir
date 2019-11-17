e19aa697da10ea5ed5ee885471ba9fc8b1abea80
Samuel Wilson <samuel.wilson@fluxfederation.com>
2019-11-17 19:43:05 +1300

    Redo grid generation as a map
    After talk with more experienced developers at the Elixir user group the
    design of the grid has been altered. A map is used as the container to
    allow more efficient use of random access algorithms. A tuple of X and Y
    coordinates serves as the key to a cell which points to a list that can
    contain links to other neighboring cells. Links could be represented as
    atoms e.g. `:north, :south, :east, :west`.
    
    Consider making a `Point` or `Coordinate` struct to enforce the
    convention of X and Y coordinates matching columns and rows
    respectively. Using these may help to reduce errors caused my reversing
    the two values.

--- lib/maze.ex
+++ lib/maze.ex
@@ -8,26 +8,38 @@ defmodule Maze do
 
   ## Examples
 
-      iex> Maze.grid(2, 3)
-      [
-        %{column: 1, links: [], row: 1},
-        %{column: 1, links: [], row: 2},
-        %{column: 1, links: [], row: 3},
-        %{column: 2, links: [], row: 1},
-        %{column: 2, links: [], row: 2},
-        %{column: 2, links: [], row: 3}
-      ]
+      iex> Maze.new(columns: 2, rows: 3)
+      %{
+        {1, 1} => [],
+        {1, 2} => [],
+        {1, 3} => [],
+        {2, 1} => [],
+        {2, 2} => [],
+        {2, 3} => [],
+      }
   """
-  def grid(columns, rows) do
-    Enum.map(
-      1..columns, fn x -> 1..rows
-        |> Enum.map(initialized_cell(x, y)) end
-      )
-      |> List.flatten
-    # Enum.map(1..columns, fn x -> 1..rows |> Enum.map(fn y -> %{column: x, row: y, links: []} end) end) |> List.flatten
+  def new(columns: columns, rows: rows) do
+    coordinates = build_coordinates(columns: columns, rows: rows)
+    build_cells(coordinates)
   end
 
-  defp initialized_cell(column, row) do
-    %{column: column, row: row, links: []}
+  defp build_coordinates(columns: columns, rows: rows) do
+    Enum.map(1..columns, fn column -> coordinates_for_column(column, 1..rows) end) |> List.flatten()
+  end
+
+  defp coordinates_for_column(column, range) do
+    Enum.map(range, fn row -> {column, row} end)
+  end
+
+  defp build_cells(coordinates) do
+    [coordinate | rest] = coordinates
+    build_cells(rest, %{coordinate => []})
+  end
+
+  defp build_cells([], cells), do: cells
+
+  defp build_cells(coordinates, cells) do
+    [coordinate | rest] = coordinates
+    build_cells(rest, Map.put(cells, coordinate, []))
   end
 end
--- test/maze_test.exs
+++ test/maze_test.exs
@@ -1,21 +1,4 @@
 defmodule MazeTest do
   use ExUnit.Case
   doctest Maze
-
-  @columns 3
-  @rows 2
-
-  test "returns a list of the correct size" do
-    maze = Maze.grid(@columns, @rows)
-    assert (length maze) == 6
-  end
-
-  test "cell coordinates are initialized" do
-    maze = Maze.grid(@columns, @rows)
-    first_cell = hd(maze)
-    last_cell = List.last(maze)
-
-    assert first_cell == %{column: 1, row: 1, links: []}
-    assert last_cell == %{column: 3, row: 2, links: []}
-  end
 end
